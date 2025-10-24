b62fabdcdd7fbd793f82252e143ffd688a610681
Coline Soleau <coline.soleau@epfedu.fr>
2025-10-24 11:22:04 +0200

    TP0_addition_SOLEAU Ajout le 24/10/2025

---
+++ TP0_addition/src/tp0_addition/TP0_addition.java
@@ -0,0 +1,42 @@
+/*
+ * TP0_helloWorld
+ * Se familiariser avec l'environnement Netbeans
+ * 20/10/2025
+ */
+package tp0_addition;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author colin
+ */
+public class TP0_addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb; // nombre d'entiers à additionner
+        int result; // resultat
+        int ind; //indice
+        
+        // initialisation
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le nombre :");
+        nb=sc.nextInt();
+
+        result=0;
+        
+        // addition des nb premiers entiers
+        ind=1;
+        
+        while (ind< nb) {
+            result=result+ind;
+            ind++;
+        }
+        // affichage du resultat
+        System.out.println("la somme des "+ nb + "entiers est:"+result); 
+        } 
+    }
+    
