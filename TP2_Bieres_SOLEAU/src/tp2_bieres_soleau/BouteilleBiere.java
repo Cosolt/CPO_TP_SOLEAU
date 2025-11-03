485323f598955a25460f12d88a1a8e0be04602db
Coline Soleau <coline.soleau@epfedu.fr>
2025-11-03 15:31:36 +0100

    03/11/2025

---
+++ TP2_Bieres_SOLEAU/src/tp2_bieres_soleau/BouteilleBiere.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_soleau;
+
+/**
+ *
+ * @author colin
+ */
+public class BouteilleBiere {
+    String Nom ;
+    double degreAlcool ;
+    String brasserie ;
+    boolean ouverte ;
+    
+    public boolean Décapsuler() {
+        if (ouverte == false){
+            ouverte = true ;
+        } else {
+            
+        }
+    }
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        Nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+  
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + Nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+    }
+    
+    
+}
---
+++ TP2_Bieres_SOLEAU/src/tp2_bieres_soleau/TP2_Bieres_SOLEAU.java
@@ -0,0 +1,27 @@
+/*
+ * TP2_Bieres_SOLEAU
+ * réaliser une première classe, créer quelques objets, et présenter
+les notions de méthodes et constructeur.
+ * 03/11/2025
+ */
+package tp2_bieres_soleau;
+
+/**
+ *
+ * @author colin
+ */
+public class TP2_Bieres_SOLEAU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson") ;
+        uneBiere.lireEtiquette();
+
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+        autreBiere.lireEtiquette();
+
+    }
+    
+}
