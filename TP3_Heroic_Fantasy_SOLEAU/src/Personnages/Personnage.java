f4a0fca83509c6f2dde91a4fd0fd003f06e41f2c
Coline Soleau <coline.soleau@epfedu.fr>
2025-11-19 11:17:22 +0100

    TP3_Heroic_Fantasy_SOLEAU 19/11/2025

--- TP3_Heroic_Fantasy_SOLEAU/src/Personnages/Personnage.java
+++ TP3_Heroic_Fantasy_SOLEAU/src/Personnages/Personnage.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personnages;
+import Armes.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,10 +13,33 @@ package Personnages;
 public abstract class Personnage {
     String nom ;
     int niveauVie ;
+    public Arme armeEquipee = null;
+    public Arme Arme_en_Main = null ;
+   
 
     public Personnage(String nom, int niveauVie) {
         this.nom = nom;
         this.niveauVie = niveauVie;
+    }
+    
+    private ArrayList<Arme> armes = new ArrayList<>() ;
+    
+    public void ajouterArme(Arme a) {
+        if (armes.size() < 5) {
+            armes.add(a);
+        }
+    }
+    
+    public void equiperArme(Arme a) {
+        for (int i = 0; i < 5; i++) {
+            if (a.equals(armes.get(i))) {  
+                armeEquipee = a;
+            }
+        }   
+    }
+
+    public Arme getArme_en_Main() {
+        return Arme_en_Main;
     }
     
     
@@ -24,5 +49,6 @@ public abstract class Personnage {
     public String toString () {
         return "Le nom du personnage est "+ nom + ", son niveau de vie est " + niveauVie ;
     }
+   
     
 }
